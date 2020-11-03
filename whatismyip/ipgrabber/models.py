22cc78d32570f8d0eaf33ed0f4cd8b594cae1fa0
fluffyhake <haakon.reppen@gmail.com>
2020-11-03 22:00:33 +0100

    fix typo on line 6

--- whatismyip/ipgrabber/models.py
+++ whatismyip/ipgrabber/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class posts(models.Model):
     title = models.CharField(max_length = 100)
-    ip = models.GenericIPAddressField())
+    ip = models.GenericIPAddressField()
     bodytext = models.TextField()
     timestamp = models.DateTimeField()
