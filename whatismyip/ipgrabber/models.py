6b05e789690cb20f5a5dbce22ea415a7fa64a347
fluffyhake <haakon.reppen@gmail.com>
2020-11-03 21:34:43 +0100

    Added ip-address field to the ipchecker app

--- whatismyip/ipgrabber/models.py
+++ whatismyip/ipgrabber/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class posts(models.Model):
+    title = models.CharField(max_length = 100)
+    ip = models.GenericIPAddressField())
+    bodytext = models.TextField()
+    timestamp = models.DateTimeField()
